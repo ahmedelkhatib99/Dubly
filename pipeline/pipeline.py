e9f779423fa3cc6d48d14bade75ed41e6f451401
AhmedIbrahimAbdellatif <Ahmed.Ahmed99@eng-st.cu.edu.eg>
2022-05-28 12:04:51 +0200

    Dubly - Add video pipeline

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -13,26 +13,31 @@ from NMT.transformer import eng_custom_standardization, spa_custom_standardizati
 from SpeechRecognizer.speech_recognizer import SpeechRecognizer
 from LipSync.inference import LipSyncing
 
+class VideoPipeline:
+    def __init__(self) -> None:
+        self.lip_syncing = LipSyncing()
+    
+    def generate_video(self, input_folder, output_folder, video_name, cloned_audio_name):
+        video_generation_progress = tqdm(range(1), desc="Generating Video", disable=False)
+        self.lip_syncing.generate_video(input_folder + '\\' + video_name, 
+                                        output_folder + "\\" + cloned_audio_name, 
+                                        output_folder + '\\' + video_name)
+        video_generation_progress.update(1)
+        video_generation_progress.close()
 
-class Pipeline:
+class SpeechToSpeechPipeline:
     def __init__(self):
-
         #################################################################### Speech Recognizer ####################################################################
         self.speech_recognizer = SpeechRecognizer()
-
-        #################################################################### Voice Cloning #############################################################################
-        device = torch.device("cpu")
         
-        print('Before Loading Encoder' ,torch.cuda.memory_allocated('cuda'))
-
+        #################################################################### Voice Cloning ####################################################################
+        device = torch.device("cpu")
         self.encoder = Encoder(device)
         self.encoder.prepare_for_inference()
 
-        print('Before Loading Synthesizer' ,torch.cuda.memory_allocated('cuda'))
         self.synthesizer = Synthesizer()
         self.synthesizer.load_model(os.path.join(os.path.dirname(__file__), "..\\TTS\\models\\synthesizer.pt"))
 
-        print('Before Loading Vocoder' ,torch.cuda.memory_allocated('cuda'))
         self.vocoder = Vocoder() 
         self.vocoder.load_model(os.path.join(os.path.dirname(__file__), "..\\TTS\\models\\vocoder.pt"))
         self.input_folder = os.path.join(os.path.dirname(__file__), "..\\demo\\input")
@@ -53,16 +58,11 @@ class Pipeline:
         audio_path = self.input_folder + "\\" + audio_filename
         assert os.path.exists(audio_path) == True, "audio file doesn't exist, please ensure that it exists in \"demo\\input\" folder!!"
         
-        print('Before Translating' ,torch.cuda.memory_allocated('cuda'))
-
         translation_progress = tqdm(range(1), desc="Translating to English", disable=False)
         english_text = translate(spanish_text, eng_custom_standardization=eng_custom_standardization, spa_custom_standardization=spa_custom_standardization)
         print(english_text)
         translation_progress.update(1)
         translation_progress.close()
-        
-        print('After Translating' ,torch.cuda.memory_allocated('cuda'))
-        
 
         speech_generation_progress = tqdm(range(4), desc="Generating English Audio (voice-cloned)", disable=False)
         embeddings = self.encoder.get_embeddings_from_audio(audio_path)
@@ -77,28 +77,27 @@ class Pipeline:
         generated_wav = self.vocoder.infer_waveform(spec)
         generated_wav = np.pad(generated_wav, (0, self.synthesizer.SAMPLING_RATE), mode="constant")
         speech_generation_progress.update(1)
+        self.vocoder.delete_model_from_memory()
+        torch.cuda.empty_cache()
 
         num_generated = len(os.listdir(self.output_folder)) + 1
-        cloned_audio_filename = "\\generated_output_%02d.wav" % num_generated
-        cloned_audio_path = self.output_folder + cloned_audio_filename
+        self.cloned_audio_filename = "\\generated_output_%02d.wav" % num_generated
+        cloned_audio_path = self.output_folder + self.cloned_audio_filename
         sf.write(cloned_audio_path, generated_wav.astype(np.float32), self.synthesizer.SAMPLING_RATE)
         speech_generation_progress.update(1)
         speech_generation_progress.close()
-        print("\n"+"="*60 + "\nSaved output in \"demo\\output\" as %s\n" % cloned_audio_filename + "="*60)
-        
-        print('Before Deleting voice cloning' ,torch.cuda.memory_allocated())
-
-        self.vocoder.delete_model_from_memory()
-        # torch.cpu.empty_cache()
-        torch.cuda.empty_cache()
-        print('After Deleting voice cloning' ,torch.cuda.memory_allocated())
+        print("\n"+"="*60 + "\nSaved output in \"demo\\output\" as %s\n" % self.cloned_audio_filename + "="*60)
         
         return self.input_folder, cloned_audio_path, video_name, self.output_folder
 
 if __name__ == "__main__":
-    print('Beginning Memory allocation' ,torch.cuda.memory_allocated('cuda'))
-    pipeline = Pipeline()
-    print('After Loading Memory allocation' ,torch.cuda.memory_allocated('cuda'))
+    speechPipeline = SpeechToSpeechPipeline()
+    video_name = "test_4.mp4"
+    speechPipeline.generate_spanish_to_english_speech(video_name)
+    input_folder = speechPipeline.input_folder 
+    output_folder = speechPipeline.output_folder 
+    cloned_audio_filename = speechPipeline.cloned_audio_filename
+    del speechPipeline
 
-    pipeline.generate_spanish_to_english_speech("test_4.mp4")
-    # pipeline.generate_spanish_to_english_speech("./videotest_2.mp4", ["Me encanta jugar con mis amigas", "me encanta mi colegio"])
+    videoPipeline = VideoPipeline()
+    videoPipeline.generate_video(input_folder, output_folder, video_name, cloned_audio_filename)
