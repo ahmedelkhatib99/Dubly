80647947c60441089e4e575d541c69153d6d8c46
Ahmed Elkhatib <ahmedkhota2010@gmail.com>
2022-06-08 00:31:51 +0200

    final

--- controller.js
+++ controller.js
@@ -1,26 +1,33 @@
 const spawn = require("child_process").spawn;
-const fs = require('fs')
+const PythonShell = require('python-shell').PythonShell;
 
 //translate
 exports.translate = async (req, res, next) => {
     try {
         let result;
-        //send filename to python file
         let filename = req.file.filename;
-        // const pythonProcess = spawn('python',["./pipeline/pipeline.py -f " + filename + " -m verbose"]);
-        const pythonProcess = spawn('python',["./pipeline/pipeline.py"]);
+        
+        //send filename to python file
+        const pythonProcess = spawn('python',["./pipeline/pipeline.py", "-f", filename, "-m", "silent"]);
+        // const pythonProcess = spawn('python',["./script.py"]);
+
+        pythonProcess.stderr.on('data', (data) => {
+            console.log(`error:${data}`);
+        });
+        pythonProcess.stderr.on('close', () => {
+            console.log("Closed");
+        })
+
         pythonProcess.stdout.on('data', (data) => {
             // Do something with the data returned from python script
-            result = data.toString();
-            console.log(data.toString())
-        });
-
-        //respond to request
-        res.status(200).json({
+            result = data.toString().trim();
+            res.status(200).json({
             Result: result,
             status: 'success',
             message: "Video translated successfuly!"
-        })
+            })
+        });
+        
     } catch (error) {
         res.status(200).json({ status: 'fail', message: error.message })
     }    
