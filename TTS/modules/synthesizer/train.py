0142132b761d1b214d852ebe1a5ffc18e95dcd01
Abdelrahman-Shahda <62548944+Abdelrahman-Shahda@users.noreply.github.com>
2022-06-17 20:50:23 +0200

    Adjusted synthesizer traning a bit more

--- TTS/modules/synthesizer/train.py
+++ TTS/modules/synthesizer/train.py
@@ -1,11 +1,14 @@
-from modules.synthesizer.synthesizer import Synthesizer
 import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), "..\\.."))
+
+from modules.synthesizer.synthesizer import Synthesizer
 
 
 if __name__ == "__main__":
     # Synthesizer.__preprocess(dataset_root, out_dir)
     synthesizer = Synthesizer()
     synthesizer.preprocess(sys.argv[1], sys.argv[2])
-    synthesizer.train()
-    synthesizer.start_training('./output','../models')
+    # synthesizer.train()
+    # synthesizer.start_training(sys.argv[2],'../../models')
     print(sys.argv)
